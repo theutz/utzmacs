366ab0d8c548716e1fb27dfa8a4973ef870ef995
Michael Utz <michael@theutz.com>
2020-10-08 01:59:12 +0300

    Get literate config setup

--- init.el
+++ init.el
@@ -1,4 +1,4 @@
-;;; Utzmacs --- Utz Certified
+;;; Utzmacs --- Utz Certified Emacs
 ;;
 ;; Copyright (C) 2020 Michael Utz
 ;;
@@ -9,67 +9,47 @@
 
 ;;; Commentary:
 ;;
-;; This is where I get everthing started.
+;; I wanted to use a literate org config.  So I did.
 ;;
 
 ;;; Code:
 
-;; Setup Straight
+(let ((gc-cons-threshold most-positive-fixnum))
+  (defvar bootstrap-version)
+  (let ((bootstrap-file
+	 (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+	(bootstrap-version 5))
+    (unless (file-exists-p bootstrap-file)
+      (with-current-buffer
+	  (url-retrieve-synchronously
+	   "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+	   'silent 'inhibit-cookies)
+	(goto-char (point-max))
+	(eval-print-last-sexp)))
+    (load bootstrap-file nil 'nomessage))
 
-(setq-default straight-fix-flycheck t
-	      straight-use-package-by-default t)
+  (straight-use-package 'org)
+  (require 'org)
 
-(defvar bootstrap-version)
+  (org-babel-load-file
+   (expand-file-name "utzmacs.org" user-emacs-directory))
 
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-	(url-retrieve-synchronously
-	 "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-	 'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(require 'straight)
-
-;; Setup Use Package
-
-(straight-use-package 'use-package)
-(require 'use-package)
-
-;; Setup Evil
-
-(use-package evil
-  :demand t
-  :functions evil-mode
-  :init
-  (setq evil-split-window-below t)
-  (setq evil-vsplit-window-right t)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-u-delete t)
-  (setq evil-want-C-w-in-emacs-state t)
-  (setq evil-shift-width 2)
-  :config
-  (evil-mode 1))
-
-(use-package flycheck
-  :functions global-flycheck-mode
-  :init
-  (setq flycheck-emacs-lisp-load-path 'inherit)
-  (global-flycheck-mode))
-
-;; Setup Which Key
-
-(use-package which-key
-  :functions which-key-mode
-  :config
-  (which-key-mode))
-
-;; Provide the Feature
+  (garbage-collect))
 
 (provide 'init)
 
 ;;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(safe-local-variable-values
+   '((eval add-to-list 'org-babel-default-header-args:emacs-lisp
+	   '(:results . "none")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
