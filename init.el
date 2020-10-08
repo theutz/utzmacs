0068037eae5b59d8530c4676fb99403a01b2a2d8
Michael Utz <michael@theutz.com>
2020-10-09 01:41:18 +0300

    Setup custom file

--- init.el
+++ init.el
@@ -39,17 +39,3 @@
 (provide 'init)
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(safe-local-variable-values
-   '((eval add-to-list 'org-babel-default-header-args:emacs-lisp
-	   '(:results . "none")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
